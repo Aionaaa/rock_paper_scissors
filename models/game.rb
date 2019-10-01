3e9cef14cdfcc0cbd7d60957fd6bbe1e21e8ec47
Iona Grant <ionagrant92@gmail.com>
2019-10-01 19:53:48 +0100

    fixed a bug

--- models/game.rb
+++ models/game.rb
@@ -3,7 +3,7 @@ class Game
   def self.game(move1, move2)
     return "draw" if move1 == move2
     return "rock wins" if move1 == "rock" && move2 == "scissors" || move1 == "scissors" && move2 == "rock"
-    return "scissors wins" if move1 == "scissors" && move2 == "paper" || move2 == "paper" && move1 == "scissors"
+    return "scissors wins" if move1 == "scissors" && move2 == "paper" || move1 == "paper" && move2 == "scissors"
     return "paper wins" if move1 == "paper" && move2 == "rock" || move1 == "rock" && move2 == "paper"
   end
 end
